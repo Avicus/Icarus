65104e0a2a130ecb74ef41942a13f83481b16eac
Austin Mayes <bookofnathan@gmail.com>
2015-07-14 11:28:40 -0500

    Check if a team has already been declared.

--- src/java/net/avicus/icarus/module/teams/TeamsParser.java
+++ src/java/net/avicus/icarus/module/teams/TeamsParser.java
@@ -11,11 +11,11 @@ import java.util.List;
 
 public class TeamsParser implements Parser<TeamsModule> {
 
+    HashMap<String, Team> teams = new HashMap<String, Team>();
+
     @Override
     public TeamsModule build(Match match) throws ParserException {
         List<ConfigSection> configs = match.getConfig().getSectionList("teams");
-
-        HashMap<String, Team> teams = new HashMap<String, Team>();
 
         for (ConfigSection cs : configs) {
             try {
@@ -29,9 +29,9 @@ public class TeamsParser implements Parser<TeamsModule> {
     }
 
     public String createIdIfAbsent(ConfigSection cs) throws ParserException {
-        if (cs.contains("id")) return cs.getAsString("id");
-        if (cs.contains("name")) return cs.getAsString("name").toLowerCase().split(" ")[0]; // "Red Team" => "red"
-        else throw new ParserException(this, "Team name must be specified."); // The team doesn't even have a name, so an error about an id is irrelevant.
+        if (cs.contains("id")  && !this.teams.containsKey(cs.getAsString("id"))) return cs.getAsString("id");
+        if (cs.contains("name") && !this.teams.containsKey(cs.getAsString("name"))) return cs.getAsString("name").toLowerCase().split(" ")[0]; // "Red Team" => "red"
+        else throw new ParserException(this, "Team name must be specified. This can also happen if a team is declared more than once."); // The team doesn't even have a name, so an error about an id is irrelevant.
     }
 
     public Team parseTeam(ConfigSection cs) throws ParserException {
