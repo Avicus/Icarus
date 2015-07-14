838cabb1b8faf01899bb5acd08d2cadf0a379a85
Austin Mayes <bookofnathan@gmail.com>
2015-07-14 02:43:16 -0500

    Add a method to create an id if "id" is not specified.

--- src/java/net/avicus/icarus/module/teams/TeamsParser.java
+++ src/java/net/avicus/icarus/module/teams/TeamsParser.java
@@ -19,13 +19,19 @@ public class TeamsParser implements Parser<TeamsModule> {
 
         for (ConfigSection cs : configs) {
             try {
-                teams.put(cs.getString("id"), parseTeam(cs));
+                teams.put(createIdIfAbsent(cs), parseTeam(cs));
             } catch (Exception e) {
                 throw new ParserException(this, "Failed to parse team '" + cs.getString("id") + "'.", e);
             }
         }
 
         return new TeamsModule(teams);
+    }
+
+    public String createIdIfAbsent(ConfigSection cs) throws ParserException {
+        if (cs.contains("id")) return cs.getAsString("id");
+        if (cs.contains("name")) return cs.getAsString("name").toLowerCase().split(" ")[0]; // "Red Team" => "red"
+        else throw new ParserException(this, "Team name must be specified."); // The team doesn't even have a name, so an error about an id is irrelevant.
     }
 
     public Team parseTeam(ConfigSection cs) throws ParserException {
